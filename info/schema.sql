db7834538f5d3a13512e8b4e02463f45394633eb
Marijn Ophorst <marijn@monomelodies.nl>
2018-04-27 08:25:25 +0200

    add schema here

---
+++ info/schema.sql
@@ -0,0 +1,15 @@
+
+CREATE SEQUENCE fakr_inbox_id_seq;
+
+CREATE TABLE fakr_inbox (
+    id BIGINT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('fakr_inbox_id_seq'::regclass),
+    sender VARCHAR(255) NOT NULL,
+    recipient VARCHAR(255) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    txt TEXT,
+    html TEXT NOT NULL,
+    datecreated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON fakr_inbox(datecreated);
+
