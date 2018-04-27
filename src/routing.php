bc0e61a0c08185095e9f6c5b90b8e62274142774
Marijn Ophorst <marijn@monomelodies.nl>
2018-04-27 08:51:44 +0200

    rename so it is more modular

--- src/routing.php
+++ src/routing.php
@@ -5,16 +5,16 @@ namespace Sensi\Fakr;
 use Zend\Diactoros\RedirectResponse;
 
 $router
-    ->when('/', 'inbox')
+    ->when('/', 'fakr-inbox')
     ->get(View::class)
     ->post(function (callable $GET) use ($router) {
         $controller = new Controller;
         foreach ($_POST['id'] as $id) {
             $controller->delete($id); 
         }
-        return new RedirectResponse($router->generate('inbox'));
+        return new RedirectResponse($router->generate('fakr-inbox'));
     });
-$router->when("/(?'id'\d+)/", 'inbox-detail')->get(function (int $id) {
+$router->when("/(?'id'\d+)/", 'fakr-message')->get(function (int $id) {
     return new Message\View($id);
 });
 
