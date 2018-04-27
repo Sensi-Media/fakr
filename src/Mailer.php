fe9928cdb7280552217e03d2f70b1b5ff3831508
Marijn Ophorst <marijn@monomelodies.nl>
2018-04-27 14:51:58 +0200

    implement normalize method

--- src/Mailer.php
+++ src/Mailer.php
@@ -4,6 +4,7 @@ namespace Sensi\Fakr;
 
 use Monolyth\Disclosure\Injector;
 use Swift_Mime_SimpleMessage;
+use DomainException;
 
 class Mailer
 {
@@ -25,6 +26,33 @@ class Mailer
         $this->sensiAdapter->insertInto('fakr_inbox')
             ->execute(compact('sender', 'recipient', 'subject', 'body'));
         return true;
+    }
+
+    /**
+     * Normalize an address. Swift gives these as either a string, or a hash of
+     * email => realname pairs. Might also contain multiple addresses.
+     *
+     * @param string|array $address
+     * @return string
+     * @throws DomainException if something non-valid was passed.
+     */
+    private function normalize($address) : string
+    {
+        if (is_string($address)) {
+            return $address;
+        }
+        if (is_array($address)) {
+            $retval = [];
+            foreach ($address as $email => $name) {
+                if (is_numeric($email)) {
+                    $retval[] = $name;
+                    continue;
+                }
+                $retval[] = "$name <$email>";
+            }
+            return implode(', ', $retval);
+        }
+        throw new DomainException;
     }
 }
 
