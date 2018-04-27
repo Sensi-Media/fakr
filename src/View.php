a8bac55943edb2f769618e17572969060e415dd6
Marijn Ophorst <marijn@monomelodies.nl>
2018-04-27 08:25:39 +0200

    setup view

---
+++ src/View.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sesni\Fakr;
+
+class View extends \View
+{
+    protected $template = 'Sensi/Fakr/template.html.twig';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->inject(function ($fakrRepository) {});
+        $this->mails = $this->fakrRepository->all();
+    }
+}
+
