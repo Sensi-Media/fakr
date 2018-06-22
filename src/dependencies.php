7fd15a35c4697d785b299b307c949ced617e0774
Marijn Ophorst <marijn@monomelodies.nl>
2018-06-22 08:54:30 +0200

    let this handle both dev and test

--- src/dependencies.php
+++ src/dependencies.php
@@ -7,29 +7,51 @@ use Monolyth\Envy\Environment;
 use Quibble\Postgresql\Adapter;
 use Quibble\Query\Buildable;
 use PDO;
+use Swift_Mime_SimpleMessage;
+use Toast\Cache\Item;
+use Toast\Cache\Pool;
 
 $container = new Container;
+$env = $container->get('env');
 
-$container->register(function (&$fakrRepository) {
-    $fakrRepository = new Repository;
-});
-$container->register(function (&$sensiAdapter) {
-    $env = new Environment(dirname(__DIR__).'/Envy.json', function ($env) {
-        return ['fakr'];
+if ($env->dev) {
+    $container->register(function (&$fakrRepository) {
+        $fakrRepository = new Repository;
     });
-    $sensiAdapter = new class(
-        'host=jerom;dbname='.$env->db['name'],
-        $env->db['user'],
-        $env->db['pass'],
-        [
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        ]
-    ) extends Adapter {
-        use Buildable;
-    };
-});
-$container->register(function (&$mailer) {
-    $mailer = new Mailer;
-});
+    $container->register(function (&$sensiAdapter) {
+        $env = new Environment(dirname(__DIR__).'/Envy.json', function ($env) {
+            return ['fakr'];
+        });
+        $sensiAdapter = new class(
+            'host=jerom;dbname='.$env->db['name'],
+            $env->db['user'],
+            $env->db['pass'],
+            [
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ]
+        ) extends Adapter {
+            use Buildable;
+        };
+    });
+    $container->register(function (&$mailer) {
+        $mailer = new Mailer;
+    });
+} elseif ($env->test) {
+    class Mailer
+    {
+        public function send(Swift_Mime_SimpleMessage $msg) : bool
+        {
+            $pool = Pool::getInstance();
+            if (!(preg_match('/To: .*? <(.*?)>/m', "$msg", $to))) {
+                preg_match('/To: (.*?)$/m', "$msg", $to);
+            }
+            $pool->save(new Item($to[1], $msg));
+            return true;
+        }
+    }
+    $container->register(function (&$mailer) {
+        $mailer = new Mailer;
+    });
+}
 
