3dc41e3c8713736b2b583f38bbd65dfb0b4f0c77
Marijn Ophorst <marijn@monomelodies.nl>
2019-09-11 15:45:48 +0200

    simplify this now we have working mail again

--- src/Mailer.php
+++ src/Mailer.php
@@ -4,55 +4,28 @@ namespace Sensi\Fakr;
 
 use Monolyth\Disclosure\Injector;
 use Swift_Message;
+use Swift_Mailer;
+use Swift_SmtpTransport;
 use DomainException;
 
 class Mailer
 {
     use Injector;
 
-    private $sensiAdapter;
+    /** @var Monolyth\Envy\Environment */
+    private $env;
     
     public function __construct()
     {
-        $this->inject(function ($sensiAdapter) {});
+        $transport = new Swift_SmtpTransport('localhost', 25);
+        parent::__construct($transport);
+        $this->inject(function ($env) {});
     }
     
     public function send(Swift_Message $msg) : bool
     {
-        $sender = $this->normalize($msg->getFrom());
-        $recipient = $this->normalize($msg->getTo());
-        $subject = $msg->getSubject();
-        $body = "$msg";
-        $this->sensiAdapter->insertInto('fakr_inbox')
-            ->execute(compact('sender', 'recipient', 'subject', 'body'));
-        return true;
-    }
-
-    /**
-     * Normalize an address. Swift gives these as either a string, or a hash of
-     * email => realname pairs. Might also contain multiple addresses.
-     *
-     * @param string|array $address
-     * @return string
-     * @throws DomainException if something non-valid was passed.
-     */
-    private function normalize($address) : string
-    {
-        if (is_string($address)) {
-            return $address;
-        }
-        if (is_array($address)) {
-            $retval = [];
-            foreach ($address as $email => $name) {
-                if (is_numeric($email)) {
-                    $retval[] = $name;
-                    continue;
-                }
-                $retval[] = "$name <$email>";
-            }
-            return implode(', ', $retval);
-        }
-        throw new DomainException;
+        $msg->setTo($this->env->mail);
+        return parent::send($msg);
     }
 }
 
