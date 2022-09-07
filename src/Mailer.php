8e4cc52446cd26552a031dca9397536f747fceab
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-07 15:02:46 +0200

    fix definitions

--- src/Mailer.php
+++ src/Mailer.php
@@ -3,8 +3,8 @@
 namespace Sensi\Fakr;
 
 use Monolyth\Disclosure\Injector;
-use Symfony\Component\Mailer\{ MailerInterface, Transport };
-use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\{ MailerInterface, Transport\TransportInterface, Envelope };
+use Symfony\Component\Mime\RawMessage;
 use DomainException;
 use Monolyth\Envy\Environment;
 
@@ -13,17 +13,19 @@ class Mailer implements MailerInterface
     use Injector;
 
     private Environment $env;
+
+    private TransportInterface $transport;
     
-    public function __construct(Transport $transport)
+    public function __construct(TransportInterface $transport)
     {
-        parent::__construct($transport);
         $this->inject(function ($env) {});
+        $this->transport = $transport;
     }
     
-    public function send(Email $msg, &$failedRecipients = null) : bool
+    public function send(RawMessage $msg, ?Envelope $envelope = null) : void
     {
         $msg->to($this->env->email);
-        return parent::send($msg);
+        $this->transport->send($msg, $envelope);
     }
 }
 
